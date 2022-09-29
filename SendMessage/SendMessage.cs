41eb3b7ad727919a2561a45e73e4b731b05cd6ed
Michael <michael@isolid.net>
2022-09-29 17:06:12 +0100

    scaffolded project

---
+++ SendMessage/SendMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace SendMessage
+{
+    public static class SendMessage
+    {
+        [FunctionName("SendMessage")]
+        public static void Run([ServiceBusTrigger("messages", Connection = "ServiceBus")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
