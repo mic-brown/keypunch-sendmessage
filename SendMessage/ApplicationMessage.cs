cb65f86333615bb42cf3011313df30106b59f930
Michael <michael@isolid.net>
2022-09-30 12:15:44 +0100

    package updates

---
+++ SendMessage/ApplicationMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SendMessage
+{
+    [JsonObject]
+    public class ApplicationMessage
+    {
+        [JsonProperty("userId")]
+        public Guid? UserId { get; set; }
+
+        [JsonProperty("arguments")]
+        public object Arguments { get; set; }
+    }
+}
--- SendMessage/SendMessage.cs
+++ SendMessage/SendMessage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Extensions.Logging;
@@ -8,10 +9,27 @@ namespace SendMessage
     public static class SendMessage
     {
         [FunctionName("SendMessage")]
-        public static void Run([ServiceBusTrigger("messages", Connection = "ServiceBus")] string myQueueItem, ILogger log)
+        public static async Task Run(
+            [ServiceBusTrigger("messages", Connection = "ServiceBus")] ApplicationMessage message,
+            [SignalR(HubName = "notifications")] IAsyncCollector<SignalRMessage> signalRMessages)
         {
-            var message = new SignalRMessage();
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            if (message.UserId.GetValueOrDefault() == Guid.Empty)
+            {
+                await signalRMessages.AddAsync(new SignalRMessage()
+                {
+                    Target="newMessage",
+                    Arguments = new[] { message.Arguments }
+                });
+            }
+            else
+            {
+                await signalRMessages.AddAsync(new SignalRMessage()
+                {
+                    Target = "newMessage",
+                    UserId = message.UserId.ToString(),
+                    Arguments = new[] { message.Arguments }
+                });
+            }
         }
     }
 }
